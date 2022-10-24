a75f1b53506c57cf66a3f7ad5a701bd2cdd863d7
nonbinary00 <agarkovaalysh12@gmail.com>
2022-10-24 23:45:38 +0200

    task 3
    Write a script that, when submitting a form, calls the createPromise(position, delay) function as many times as you enter in the amount field. At each call, pass it the number of the generated promise (position) and the delay, taking into account the first delay (delay) and step (step) entered by the user.

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,28 @@
+// 
+import Notiflix from 'notiflix';
+
+//Refs
+const { delayValue, stepValue, amountValue, formEl } = {
+  delayValue: document.querySelector("[name='delay']"),
+  stepValue: document.querySelector("[name='step']"),
+  amountValue: document.querySelector("[name='amount']"),
+  formEl: document.querySelector('.form'),
+};
+
+
+
+
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+  return promise;
 }
