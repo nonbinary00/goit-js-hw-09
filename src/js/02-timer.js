72cdfb3bc699f6e66263b5f56d765274660132bf
nonbinary00 <agarkovaalysh12@gmail.com>
2022-10-24 17:34:02 +0200

    task 2 (choose a date)
    The onClose() method from the options object is called each time the UI element that flatpickr creates is closed. It is in it that the date selected by the user should be processed. The selectedDates parameter is an array of selected dates, so we take the first element.
    
    If the user has selected a date in the past, show window.alert() with the text "Please choose a date in the future".
    If the user has selected a valid date (in the future), the "Start" button becomes active.
    The "Start" button should be disabled until the user has selected a date in the future.
    When you click on the "Start" button, the countdown to the selected date starts from the moment you click it.

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,3 +1,36 @@
 import flatpickr from 'flatpickr';
 import "flatpickr/dist/flatpickr.min.css";
 import Notiflix from 'notiflix';
+
+const refs = {
+    input: document.querySelector('#datetime-picker'),
+    startBtn: document.querySelector('button[data-start]'),
+    daysEl: document.querySelector('span[data-days]'),
+    hoursEl: document.querySelector('span[data-hours]'),
+    minutesEl: document.querySelector('span[data-minutes]'),
+    secondsEl: document.querySelector('span[data-seconds]'),
+  };
+
+  refs.startBtn.setAttribute('disabled', 'disabled');
+
+  const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    static: true,
+    onClose(selectedDates) {
+      console.log(selectedDates[0]);
+      if (selectedDates[0] >= Date.now()) {
+        refs.startBtn.removeAttribute('disabled');
+      } else {
+        Notiflix.Notify.failure('Please choose a date in the future');
+        refs.startBtn.setAttribute('disabled', 'disabled');
+      }
+    },
+  };
+
+  flatpickr('#datetime-picker', options);
+
+
+  
