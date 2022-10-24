4bf05115b9764695d459c2ad5417e52140004dcd
nonbinary00 <agarkovaalysh12@gmail.com>
2022-10-24 18:46:46 +0200

    task 2
    Function to convert milliseconds to object of days,hours,minutes,seconds

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -27,24 +27,66 @@ const {
 //Initialize id for interval
 let intervalId;
 
-  //Options for flatpickr
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-      //Reject any date before or equal to Date.now()
-      //Using Notify for alert
-      //Disables start button if user changed date from future to before
-      if (Date.now() - selectedDates[0].getTime() >= 0) {
-        addAttributeDisabled(startBtn, true);
+	enableTime: true,
+	time_24hr: true,
+	defaultDate: new Date(),
+	minuteIncrement: 1,
+	onClose(selectedDates) {
+		console.log(selectedDates[0]);
+		if (Date.now() > selectedDates[0].getTime()) {
+			Notify.failure("Please choose a date in the future");
+			addAttributeDisabled(startBtn, true);
+			return;
+		}
+		addAttributeDisabled(startBtn, false);
+	},
+};
+
+
+//Initialize flatpickr
+const fp = flatpickr('#datetime-picker', options);
+
+//Event to start timer, change markup
+
+startBtn.addEventListener('click', () => {
+    addAttributeDisabled(startBtn, true);
   
-        Notify.failure('Please choose a date in the future');
+    intervalId = setInterval(() => {
+      const timeDelta = fp.selectedDates[0].getTime() - Date.now();
+      //To stop timer when it hits 0
+      if (timeDelta <= 0) {
+        clearInterval(intervalId);
         return;
       }
-  
-      addAttributeDisabled(startBtn, false);
-    },
-  };
+      const convertedDelta = convertMs(timeDelta);
 
+      changeTextContent(daysValue, convertedDelta, 'days');
+      changeTextContent(hoursValue, convertedDelta, 'hours');
+      changeTextContent(minutesValue, convertedDelta, 'minutes');
+      changeTextContent(secondsValue, convertedDelta, 'seconds');
+  
+      console.log(convertedDelta);
+    }, 1000);
+  });
+
+
+  //Function to convert milliseconds to object of days,hours,minutes,seconds
+function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  
+    return { days, hours, minutes, seconds };
+  }
