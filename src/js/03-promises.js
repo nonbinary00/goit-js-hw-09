10dad89e57f3526934e92664e0ebd329d3984299
nonbinary00 <agarkovaalysh12@gmail.com>
2022-10-25 00:02:28 +0200

    task 3
    1. There is a function createPromise(position, delay) - IMPORTANT - it must RETURN a new Promise
    2. In the body of the promise, there should be a delay in calling the resolve and reject callbacks, which is specified through the delay parameter from the function (hint - setTimeout).
    3. The then and catch methods need to be attached to the body of the promise, which will be executed with arguments from the resolve and reject callbacks. The argument that in resolve, that in reject will be the object { position, delay }, and then and catch will execute their callback with the arguments passed to them (in the task, the callback of the methods inside then and catch will be console.log)
    4. That's all, you wrote a promise, the result of which we will receive through a time delay, it's ready.
    5. Next, we need to think about how to invoke this promise the required number of times.
     6. We thought about it, came up with it - well, then, on each iteration of the call to the createPromise(position, delay) function, you need to add +1 to position, and +delay to delay from the “Delay step” field.
     7. Then, it will be necessary to take into account the condition with first delay, but here there is literally one if for 3 lines of code.
    
     1. Есть функция createPromise(position, delay) – ВАЖНО – она должна ВЕРТАТЬ новый Promise
    2. В теле промиса, должна быть задержка вызова колбеков resolve и reject, которая задается через параметр delay из функции (подсказка – setTimeout).
    3. К телу промиса следует прицепить методы then и catch, которые будут выполняться с аргументами из колбеков resolve и reject. Аргументом что в resolve, что в reject будет объект {position, delay}, а then и catch будут выполнять свой колбек с переданными им аргументами (в задании колбеком методов внутри then i catch будет console.log)
     4. Все, вы написали промисс, результат выполнения которого мы получим через delay времени, это готово.
     5. Далее нужно подумать, как нам вызвать этот промисс нужное количество раз.
    6. Подумали, придумали – хорошо, затем, на каждой итерации вызова функции createPromise(position, delay), нужно добавлять +1 к position, и +delay к delay из поля “Delay step”.
    7. Затем, нужно будет учесть еще условие из first delay, но здесь буквально один if на 3 срока кода.

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,28 +1,64 @@
 // 
 import Notiflix from 'notiflix';
 
-//Refs
-const { delayValue, stepValue, amountValue, formEl } = {
-  delayValue: document.querySelector("[name='delay']"),
-  stepValue: document.querySelector("[name='step']"),
-  amountValue: document.querySelector("[name='amount']"),
-  formEl: document.querySelector('.form'),
-};
-
-
-
-
-
+const delay = document.querySelector('input[name="delay"]');
+const step = document.querySelector('input[name="step"]');
+const amount = document.querySelector('input[name="amount"]');
+const btnSubmit = document.querySelector('button[type="submit"]');
+//console.log(btnSubmit);
 
 function createPromise(position, delay) {
-  const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {const shouldResolve = Math.random() > 0.3;
-      if (shouldResolve) {
-        resolve({ position, delay });
-      } else {
-        reject({ position, delay });
-      }
-    }, delay);
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const shouldResolve = Math.random() > 0.3;
+        if (shouldResolve) {
+          resolve({ position, delay });
+        } else {
+          reject({ position, delay });
+        }
+      }, delay);
+    });
+    return promise;
+  }
+  
+  btnSubmit.addEventListener('click', e => {
+    e.preventDefault();
+    let firstDelay = Number(delay.value);
+    let delayStep = Number(step.value);
+    for (let i = 0; i < amount.value; i++) {
+      createPromise(1 + i, firstDelay + i * delayStep)
+        .then(({ position, delay }) => {
+          Notiflix.Notify.success(
+            `✅ Fulfilled promise ${position} in ${delay}ms`
+          );
+        })
+        .catch(({ position, delay }) => {
+          Notiflix.Notify.failure(
+            `❌ Rejected promise ${position} in ${delay}ms`
+          );
+        });
+    }
   });
-  return promise;
-}
+
+// btnSubmit.addEventListener('click', e => {
+//   e.preventDefault();
+//   let firstDelay = Number(delay.value);
+//   let delayStep = Number(step.value);
+//   for (let i = 0; i < amount.value; i++){
+//     createPromise(1 + i, firstDelay + i * delayStep)
+//       .then(({ position, delay }) => {
+//         console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//       })
+//       .catch(({ position, delay }) => {
+//         console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
+//   }
+// })
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
