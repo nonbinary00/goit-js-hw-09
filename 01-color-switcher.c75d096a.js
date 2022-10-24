639b720bdb22c9e462eae1b977c518c60cf72dc0
nonbinary00 <106620930+nonbinary00@users.noreply.github.com>
2022-10-24 13:51:55 +0000

    Deploying to gh-pages from @ nonbinary00/goit-js-hw-09@9c915d2eb40b87b95a74661a3831460b70deccc1 🚀

---
+++ 01-color-switcher.c75d096a.js
@@ -0,0 +1,2 @@
+!function(){var t={body:document.querySelector("body"),startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")};t.startBtn.addEventListener("click",startChangeColor),t.stopBtn.addEventListener("click",stopChangeColor)}();
+//# sourceMappingURL=01-color-switcher.c75d096a.js.map
