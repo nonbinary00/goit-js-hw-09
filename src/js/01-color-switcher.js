2baffa931662c7748e5a7a241fd4f921bd804985
nonbinary00 <agarkovaalysh12@gmail.com>
2022-10-24 16:01:47 +0200

    added functions to the start and stop buttons

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -10,3 +10,24 @@ const refs = {
 refs.startBtn.addEventListener('click', startChangeColor);
 refs.stopBtn.addEventListener('click', stopChangeColor);
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+  function changeColor() {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }
+
+  function startChangeColor() {
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+    interval = setInterval(changeColor, 1000);
+    
+  }
+
+  function stopChangeColor() {
+    clearInterval(interval);
+    refs.startBtn.disabled = false;
+    refs.stopBtn.disabled = true;
+    
+  }
