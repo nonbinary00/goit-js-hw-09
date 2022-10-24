9c915d2eb40b87b95a74661a3831460b70deccc1
nonbinary00 <agarkovaalysh12@gmail.com>
2022-10-24 15:51:31 +0200

    task 1
    to reach the button found buttons by date attribute
    
    declared a new variable and added a listener to the button

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,12 @@
+const refs = {
+    body: document.querySelector('body'),
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+  };
+
+  let interval = null
+
+
+refs.startBtn.addEventListener('click', startChangeColor);
+refs.stopBtn.addEventListener('click', stopChangeColor);
 
